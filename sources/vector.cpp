8379e935cedef496410aebd35a9c04552d47522a
amirnyy <37066891+amirnyy@users.noreply.github.com>
2018-03-28 17:14:23 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -12,33 +12,63 @@ vector_t::vector_t()
 
 vector_t::vector_t(vector_t const & other)
 {
-	elements_=other.elements_;
 	size_=other.size_;
-	capacity_=other.capacity_
+	capacity_=other.capacity_;
+	elements_= new int [capacity_];
+	for(unsigned int i = 0; i<size_;i++)
+	{
+		elements_[i]=other.elements_[i];
+	}
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
+	if (this->elements_==other.elements_) return *this;
+	else 
+	{
+		delete [] elements_;
+		size_= other.size_;
+		capacity_ = other.capacity_;
+		elements_ = new int [capacity_];
+		for (unsigned int i=0; i< size_; i++) 
+		{
+			elements_[i] =other.elements_[i];
+		}
+	}
 	return *this;
 }
 
 bool vector_t::operator ==(vector_t const & other) const
 {
-	return false;
+	bool flag=true;
+	if (size_!=other.size_) flag=1;
+	else 
+	{
+		for (unsigned int i=0; i< size_; i++) 
+		{
+		if (elements_[i] !=other.elements_[i]) flag =false;
+		}
+	}
+	return flag;
 }
 
 vector_t::~vector_t()
 {
+	if (elements_ != nullptr) 
+		delete [] elements_;
+	size_=0;
+	capacity_=0;
+	
 }
 
 std::size_t vector_t::size() const
 {
-    return 0;
+    return size_;
 }
 
 std::size_t vector_t::capacity() const
 {
-    return 0;
+    return capacity_;
 }
 
 void vector_t::push_back(int value)
